fe7f021533a31b88aef6b53e007aa30b994eaeee
yeongbinim <yeim.dev@gmail.com>
2025-02-06 08:42:16 +0900

    test

--- src/main/java/yeim/publish/TempController.java
+++ src/main/java/yeim/publish/TempController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TempController {
 
+    //test
     @GetMapping("/temp")
     public ResponseEntity<String> temp() {
         return ResponseEntity.ok("temp");

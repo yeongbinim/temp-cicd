63a1b6389369f98ab57aad8e4cf987020b370cc1
yeongbinim <yeim.dev@gmail.com>
2025-02-06 08:06:01 +0900

    temp

---
+++ src/main/java/yeim/publish/TempController.java
@@ -0,0 +1,14 @@
+package yeim.publish;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TempController {
+
+    @GetMapping("/temp")
+    public ResponseEntity<String> temp() {
+        return ResponseEntity.ok("temp");
+    }
+}
